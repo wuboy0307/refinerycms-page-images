c9d60742078d73263a9bcbc2d9d9feeee688b783
wuboy <wuboys@gmail.com>
2014-03-16 09:28:36 +0800

    for crazyjoker

--- db/migrate/20130701083432_add_tag_to_image_pages.rb
+++ db/migrate/20130701083432_add_tag_to_image_pages.rb
@@ -2,5 +2,6 @@ class AddTagToImagePages < ActiveRecord::Migration
   def change
     add_column Refinery::ImagePage.table_name, :tag, :string
     add_column Refinery::ImagePage.table_name, :part_title, :string
+    add_column Refinery::ImagePage.table_name, :link_url, :string
   end
 end
--- lib/refinery/page_images/extension.rb
+++ lib/refinery/page_images/extension.rb
@@ -2,12 +2,8 @@ module Refinery
   module PageImages
     module Extension
       def has_many_page_images
-        has_many :image_pages, :as => :page, :class_name => 'Refinery::ImagePage', :order => 'position ASC', :include => :image
-        has_many :images, :through => :image_pages, :class_name => 'Refinery::Image', :order => 'position ASC' do
-          def part(part_title="-1")
-            where("LOWER(#{Refinery::ImagePage.table_name}.part_title)=?", part_title.to_s.downcase)
-          end
-        end
+        has_many :image_pages, proc { order('position ASC') }, :as => :page, :class_name => 'Refinery::ImagePage'
+        has_many :images, proc { order('position ASC') }, :through => :image_pages, :class_name => 'Refinery::Image'
         # accepts_nested_attributes_for MUST come before def images_attributes=
         # this is because images_attributes= overrides accepts_nested_attributes_for.
 
@@ -17,33 +13,35 @@ module Refinery
         # deletes an already defined images_attributes
         module_eval do
           def images_attributes=(data)
+            data = data.reject {|key, data| data.blank?}
             ids_to_keep = data.map{|i, d| d['image_page_id']}.compact
 
             image_pages_to_delete = if ids_to_keep.empty?
-              self.image_pages
-            else
-              self.image_pages.where(
-                Refinery::ImagePage.arel_table[:id].not_in(ids_to_keep)
-              )
-            end
+                                      self.image_pages
+                                    else
+                                      self.image_pages.where(
+                                          Refinery::ImagePage.arel_table[:id].not_in(ids_to_keep)
+                                      )
+                                    end
 
             image_pages_to_delete.destroy_all
 
             data.each do |i, image_data|
-              image_page_id, image_id, caption, tag, part_title =
-                image_data.values_at('image_page_id', 'id', 'caption', "tag", "part_title")
+              image_page_id, image_id, caption, tag, part_title, link_url =
+                  image_data.values_at('image_page_id', 'id', 'caption', "tag", "part_title", "link_url")
 
               next if image_id.blank?
 
               image_page = if image_page_id.present?
-                self.image_pages.find(image_page_id)
-              else
-                self.image_pages.build(:image_id => image_id)
-              end
+                             self.image_pages.find(image_page_id)
+                           else
+                             self.image_pages.build(:image_id => image_id)
+                           end
 
               image_page.position = i
               image_page.caption = caption if Refinery::PageImages.captions
               image_page.tag = tag
+              image_page.link_url = link_url
               image_page.part_title = part_title
               image_page.save
             end
@@ -69,6 +67,10 @@ module Refinery
 
         def part_for_image_index(index)
           self.image_pages[index].try(:part_title).presence || ""
+        end
+
+        def link_url_for_image_index(index)
+          self.image_pages[index].try(:link_url).presence || ""
         end
       end
     end
